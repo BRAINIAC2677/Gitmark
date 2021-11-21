002aa5907a577cb307fcb197d5f989ae31895f74
Asif Azad <66885542+BRAINIAC2677@users.noreply.github.com>
2021-11-21 23:43:41 +0600

    css done and remove functinality added.

--- gitmark/content.js
+++ gitmark/content.js
@@ -1,34 +1,43 @@
 //adding gitmark button to the page
 let heading = document.querySelector("h1");
 let gitmarkButton = document.createElement("button");
-gitmarkButton.innerHTML = "Gitmark";
 gitmarkButton.classList.add("gitmark-btn");
 heading.appendChild(gitmarkButton);
 
-gitmarkButton.addEventListener("click", function () {
-  chrome.storage.sync.get(["repos"], function (result) {
-    let repos = result.repos;
-    if (repos == undefined) {
-      repos = [];
-    }
-    if (repos.indexOf(window.location.href) == -1) {
-      repos.push(window.location.href);
-      chrome.storage.sync.set({ repos: repos }, function () {
-        console.log("repos added");
-        console.log(repos);
-      });
-    }
-  });
+//marking the gitmark icon if the repos is gitmark
+chrome.storage.sync.get(["repos"], function (result) {
+  if (result.repos.includes(window.location.href)) {
+    gitmarkButton.classList.add("filled");
+  }
 });
+
+//adding/removing the repo on clicking gitmark icon
+gitmarkButton.addEventListener("click", onGitmark);
+function onGitmark() {
+  chrome.storage.sync.get(["repos"], function (result) {
+    if (result.repos.includes(window.location.href)) {
+      result.repos.splice(result.repos.indexOf(window.location.href), 1);
+      gitmarkButton.classList.remove("filled");
+    } else {
+      result.repos.push(window.location.href);
+      gitmarkButton.classList.add("filled");
+    }
+    chrome.storage.sync.set({ repos: result.repos }, function () {
+      console.log("repos updated");
+    });
+  });
+}
 
 //adding popup button to the page
 let popupButton = document.createElement("button");
 popupButton.innerHTML = "Popup";
 popupButton.classList.add("popup-btn");
-heading.appendChild(popupButton);
+document.body.appendChild(popupButton);
 
+//opening the popup on clicking popup button
 popupButton.addEventListener("click", () => {
-  gitmarkView.classList.toggle("hide");
+  gitmarkView.classList.toggle("show");
+  popupButton.classList.toggle("popup-btn-clicked");
 });
 
 let gitmarkView = document.createElement("div");
@@ -53,7 +62,19 @@ chrome.storage.sync.get(["repos"], function (result) {
     let url = new URL(repo);
     let repoName = url.pathname.split("/")[2];
     let authorName = url.pathname.split("/")[1];
-    gitmarkItem.innerHTML = `<a href="${repo}">${repoName}</a><p>by ${authorName}<p/>`;
+    gitmarkItem.innerHTML = `<div><a href="${repo}">${repoName}</a><p>by ${authorName}</p></div>`;
+    let mark = document.createElement("button");
+    mark.classList.add("gitmark-btn", "filled");
+
+    mark.addEventListener("click", () => {
+      repos.splice(repos.indexOf(repo), 1);
+      chrome.storage.sync.set({ repos: repos }, function () {
+        console.log("repos updated");
+      });
+      gitmarkItem.remove();
+    });
+
+    gitmarkItem.appendChild(mark);
     gitmarkView.appendChild(gitmarkItem);
   }
 });
